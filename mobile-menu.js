3e339d863f751f3bc636464a2d60f016ed13b417
LiudmylaBolotova <liudmylabolotova@gmail.com>
2024-01-03 23:01:40 +0200

    Final version
    Adds styles, fixes bugs

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,35 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleListButton = document.getElementById('toggleListButton');
-  const closeListButton = document.getElementById('closeListButton');
-  const listContainer = document.getElementById('listContainer');
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleListButton = document.getElementById("toggleListButton");
+  const closeListButton = document.getElementById("closeListButton");
+  const listContainer = document.getElementById("listContainer");
 
-  toggleListButton.addEventListener('click', function () {
-    if (listContainer.style.display === 'none' || listContainer.style.display === '') {
-      listContainer.style.display = 'block';
-      // toggleListButton.style.display = 'none';
-      closeListButton.style.display = 'block';
+  toggleListButton.addEventListener("click", function () {
+    if (
+      listContainer.style.display === "none" ||
+      listContainer.style.display === ""
+    ) {
+      listContainer.style.display = "block";
+      toggleListButton.style.display = "none";
+      closeListButton.style.display = "block";
     } else {
-      listContainer.style.display = 'none';
-      toggleListButton.style.display = 'block';
-      closeListButton.style.display = 'none';
+      listContainer.style.display = "none";
+      toggleListButton.style.display = "block";
+      closeListButton.style.display = "none";
     }
   });
 
-  closeListButton.addEventListener('click', function () {
-    listContainer.style.display = 'none';
-    toggleListButton.style.display = 'block';
-    closeListButton.style.display = 'none';
+  closeListButton.addEventListener("click", function () {
+    listContainer.style.display = "none";
+    toggleListButton.style.display = "block";
+    closeListButton.style.display = "none";
   });
-
-  // window.addEventListener('resize', function () {
-  //   if (window.innerWidth < 992) {
-      // listContainer.style.display = 'block';
-      // toggleListButton.style.display = 'none';
-      // closeListButton.style.display = 'block';
-  //   } else {
-  //     listContainer.style.display = 'none';
-  //     toggleListButton.style.display = 'block';
-  //     closeListButton.style.display = 'none';
-  //   }
-  // });
 });
